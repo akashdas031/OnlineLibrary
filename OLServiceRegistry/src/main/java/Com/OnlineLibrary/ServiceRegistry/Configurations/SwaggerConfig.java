5a50147603c499b7aec91cd4ff8a9134e614c045
Akash Das <128533258+akashdas031@users.noreply.github.com>
2025-05-03 18:46:23 +0530

    Add files via upload
    Swagger Documentation Implemented

---
+++ OLServiceRegistry/src/main/java/Com/OnlineLibrary/ServiceRegistry/Configurations/SwaggerConfig.java
@@ -0,0 +1,21 @@
+package Com.OnlineLibrary.ServiceRegistry.Configurations;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+
+@Configuration
+public class SwaggerConfig {
+
+	@Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI()
+            .info(new Info()
+                .title("Service Registry Online Library")
+                .version("1.0")
+                .description("API documentation for Service Registry"));
+    }
+}
